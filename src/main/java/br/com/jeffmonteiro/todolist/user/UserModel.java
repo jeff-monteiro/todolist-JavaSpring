d4cb23598ade699e4b82c10018db85afb7dfd91c
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2024-11-19 21:02:57 -0300

    feat:creating_table_with_@Entity

--- src/main/java/br/com/jeffmonteiro/todolist/user/UserModel.java
+++ src/main/java/br/com/jeffmonteiro/todolist/user/UserModel.java
@@ -1,11 +1,21 @@
 package br.com.jeffmonteiro.todolist.user;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")// Assigning a name to the my table.
 public class UserModel {
+
+    @Id // Private key in my table
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
 }
