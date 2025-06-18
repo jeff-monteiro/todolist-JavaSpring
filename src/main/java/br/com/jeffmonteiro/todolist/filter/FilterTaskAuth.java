b3ba8f90cac13f753f0a900a659c9116bbfbc529
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-06-18 17:21:46 -0300

    UpdateFilterTask

--- src/main/java/br/com/jeffmonteiro/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/jeffmonteiro/todolist/filter/FilterTaskAuth.java
@@ -1,16 +1,28 @@
 package br.com.jeffmonteiro.todolist.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Implement your authentication logic here
+        String getAuth = request.getHeader("Authorization");
 
-        filterChain.doFilter(servletRequest, servletResponse);
+        String getPassword = getAuth.substring("Basic".length()).trim();
+        System.out.println("Authorization Header" + getPassword);
+
+        // If authentication fails, you can send an error response
+        // response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+
+        // If authentication succeeds, continue with the filter chain
+        filterChain.doFilter(request, response);
     }
 }
