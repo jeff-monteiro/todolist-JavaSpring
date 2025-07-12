90656ee0575a5b94bd9529c7be812b7885daa344
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-07-12 15:10:30 -0300

    setting up attribute on request

--- src/main/java/br/com/jeffmonteiro/todolist/task/TaskController.java
+++ src/main/java/br/com/jeffmonteiro/todolist/task/TaskController.java
@@ -1,10 +1,14 @@
 package br.com.jeffmonteiro.todolist.task;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,7 +19,10 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel){
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        System.out.println("Chegou no controller");
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     }
