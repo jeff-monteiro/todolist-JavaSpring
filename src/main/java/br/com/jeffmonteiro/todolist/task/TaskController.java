322e722eab7ccc830fa2e492da77841de64aa9b7
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-07-20 18:21:37 -0300

    feat: list of task by userId

--- src/main/java/br/com/jeffmonteiro/todolist/task/TaskController.java
+++ src/main/java/br/com/jeffmonteiro/todolist/task/TaskController.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -37,6 +35,14 @@ public class TaskController {
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    // Getting all tasks for the only userId
+    @GetMapping("/")
+    public List<TaskModel> listTasks(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
     }
     
 }
